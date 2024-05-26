add26c6bd8df10dfb69065d0fd68faeb6fa03284
tcedro <thiago.cedro@sga.pucminas.brgit config --global user.name tcedrogit config --global user.email thiago.cedro@sga.pucminas.br>
2024-05-26 17:12:37 -0300

    email as service w/ userService

--- src/main/java/com/notificaClima/App/services/UserService.java
+++ src/main/java/com/notificaClima/App/services/UserService.java
@@ -16,26 +16,30 @@ public class UserService {
     
     @Autowired
     private UserRepository userRepository;
+
     public List<UserDTO> listAll() {
         List<UserEntity> users = userRepository.findAll();
         return users.stream().map(UserDTO::new).toList();
     }
     public void insert(UserDTO user) {
         UserEntity userEntity = new UserEntity(user);
+        String text = "Parabens " + user.getName() + ", você acabou de se registrar em nosso site.";
+        emailService.sendText("New Account", text, user.getLogin());
         userRepository.save(userEntity);
     }
     public UserDTO edit(UserDTO user) {
         UserEntity userEntity = new UserEntity(user);
+        String text = user.getName() + ", você acabou de editar seu perfil no nosso site.";
+        emailService.sendText("Edit Account", text, user.getLogin());
         return new UserDTO(userRepository.save(userEntity));
     }
     public void delete(Long id) {
         UserEntity userEntity = userRepository.getReferenceById(id);
+        String text = userEntity.getName() + ", seu perfil foi deletado em nosso site.";
+        emailService.sendText("New Account", text, userEntity.getLogin());
         userRepository.delete(userEntity);
     }
     public UserDTO searchById(Long id) {
         return new UserDTO(userRepository.findById(id).get());
     }
-    // public String sendText() {
-    //     return emailService.sendText("Nota da Prova", "Sua nota de PO foi 17/20", "maria.kos@sga.pucminas.br");
-    // }
 }
